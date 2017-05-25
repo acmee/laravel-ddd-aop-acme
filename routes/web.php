813939e1fb5ea36c6d7da18ebc856bf51dc8d666
Ulf Tiburtius <ulf@idea-works.de>
2017-05-25 14:41:48 +0200

    update web route configuration

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
+use \Illuminate\Routing\Router;
+
+/** @var \Illuminate\Routing\Router $router */
+$router->group(
+        ['as' => 'home::'],
+        function (Router $router) : void {
+            $router->get('/', [
+                'as'   => 'index',
+                'uses' => 'HomeController@index'
+            ]
+        );
+   }
+);
