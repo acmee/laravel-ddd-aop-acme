5ef677066df890cccbba166ceb755aa7f010d842
Ulf Tiburtius <ulf@idea-works.de>
2017-05-25 14:38:37 +0200

    php7 formatting

--- app/helpers.php
+++ app/helpers.php
@@ -2,30 +2,45 @@
 
 if (!function_exists('get_tld'))
 {
-    function get_tld()
+    /**
+     * @return string
+     */
+    function get_tld() : string
     {
-        return substr(\Illuminate\Support\Facades\Request::root(), strrpos(\Illuminate\Support\Facades\Request::root(), '.') + 1 );
+        return \substr(\Illuminate\Support\Facades\Request::root(), strrpos(\Illuminate\Support\Facades\Request::root(), '.') + 1 );
     }
 }
 
 if (!function_exists('route_rel'))
 {
-    function route_rel($name, array $parameters = [])
+    /**
+     * @param string $name
+     * @param array $parameters
+     *
+     * @return string
+     */
+    function route_rel(string $name, array $parameters = []) : string
     {
-        return route($name, $parameters, false);
+        return \route($name, $parameters, false);
     }
 }
 
 if (!function_exists('array_multi_get'))
 {
-    function array_multi_get(array $array, $key)
+    /**
+     * @param array $array
+     * @param string $key
+     *
+     * @return array
+     */
+    function array_multi_get(array $array, string $key) : array
     {
         $result = [];
         foreach ($array as $item) {
-            if (is_array($item)) {
-                $result[] = array_get($item, $key);
+            if (\is_array($item)) {
+                $result[] = \array_get($item, $key);
             }
         }
-        return array_filter($result);
+        return \array_filter($result);
     }
 }
