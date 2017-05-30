a1e69859fbcc8ad970fa3570a567c05bda5b15a5
Ulf Tiburtius <ulf@idea-works.de>
2017-05-30 12:42:20 +0200

    register aop
    - known issues if running with php artisan serve
    — storage permissions
    — unknown route no pretty error handler

---
+++ app/Providers/AopServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Acme\Providers;
+
+use Acme\Infrastructure\Aspects\Logging\LoggingAspect;
+use Go\Laravel\GoAopBridge\GoAopServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class AopServiceProvider
+ *
+ * @package Acme\Providers
+ * @author Ulf Tiburtius <ulf@idea-works.de>
+ * @since 05/24/2017
+ */
+class AopServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string
+     */
+    private $aopTag = 'goaop.aspect';
+
+    /**
+     * @return void
+     */
+    public function register() : void
+    {
+        $this->app->register(GoAopServiceProvider::class);
+
+        $this->app->singleton(LoggingAspect::class, function (Application $app) {
+            return new LoggingAspect($app->make(LoggerInterface::class));
+        });
+
+        $this->app->tag([LoggingAspect::class], $this->aopTag);
+    }
+
+    /**
+     * @return array
+     */
+    public function provides() : array
+    {
+        return [
+            LoggingAspect::class
+        ];
+    }
+}
